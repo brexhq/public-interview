edd3307a9a4340268dcddfce12daee8b15d178f4
Alvin Sng <alvin@brex.com>
2020-12-17 17:35:29 -0800

    Create HelloWorld test program

---
+++ eng-debugging/hello_world_java/src/main/java/HelloWorld.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class HelloWorld {
+    private final String filePath;
+
+    HelloWorld(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String loadFile() throws IOException {
+        URL file = this.getClass().getClassLoader().getResource(filePath);
+        String path = new File(file.getPath()).getAbsolutePath();
+        return new String(Files.readAllBytes(Paths.get(path)), StandardCharsets.UTF_8);
+    }
+
+    public String run() throws IOException {
+        String contents = loadFile();
+        // To fix test case 2, change the line below.
+        return contents.replace('_', ' ');
+    }
+}
---
+++ eng-debugging/hello_world_javascript/README.md
@@ -0,0 +1,10 @@
+### Setup
+1. If you don't have nodejs installed, run `brew install node` for Mac. See https://nodejs.org/en/download/ for other OS
+2. run `npm install` inside this folder
+3. run `npm test` to run all unit tests
+4. run `npm run test-only 1` to just run the first test
+
+
+## Tested on
+node v12.6.0
+npm v6.13.2
