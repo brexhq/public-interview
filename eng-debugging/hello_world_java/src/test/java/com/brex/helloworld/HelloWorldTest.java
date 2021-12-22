adddb74ea39b9eadee737f57296ddeecb2881980
Nikunj Yadav <nikunj@brex.com>
2021-12-22 15:38:30 -0800

    Include syntax that requires java language level >= 11

--- eng-debugging/hello_world_java/src/test/java/com/brex/helloworld/HelloWorldTest.java
+++ eng-debugging/hello_world_java/src/test/java/com/brex/helloworld/HelloWorldTest.java
@@ -16,14 +16,14 @@ class HelloWorldTest {
     void testCase1() throws IOException {
       // This test case is passing
       HelloWorld helloWorld = new HelloWorld("input/test_1.txt");
-      String actualOutput = helloWorld.run();
+      String actualOutput = helloWorld.run("user-4");
       assertEquals("hello world\n", actualOutput);
     }
     @org.junit.jupiter.api.Test
     void testCase2() throws IOException {
       // This test case is failing
       HelloWorld helloWorld = new HelloWorld("input/test_2.txt");
-      String actualOutput = helloWorld.run();
+      String actualOutput = helloWorld.run("user-5");
       assertEquals("hello-world\n", actualOutput);
     }
 }
